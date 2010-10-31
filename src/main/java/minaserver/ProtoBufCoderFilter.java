c644485ebe39dc9e9e2d2247147854e44afe4f8f
Your Name <alexander.schrab@gmail.com>
2010-10-31 14:25:29 +0100

    Removed old files

--- src/main/java/minaserver/ProtoBufCoderFilter.java
+++
@@ -1,9 +0,0 @@
-package minaserver;
-
-import org.apache.mina.filter.codec.ProtocolCodecFilter;
-
-public class ProtoBufCoderFilter extends ProtocolCodecFilter {
-	public ProtoBufCoderFilter(ProtoBufMessageFactory protoBufMessageFactory) {
-		super(new ProtoBufCoderFactory(protoBufMessageFactory));
-	}
-}
--- src/main/java/minaserver/ProtoBufMessageFactory.java
+++
@@ -1,8 +0,0 @@
-package minaserver;
-
-import minaserver.Test.Person.Builder;
-
-//Factory creating protobuf builders for the protobuf decoder 
-public interface ProtoBufMessageFactory {
-	Builder createProtoBufMessage();
-}
